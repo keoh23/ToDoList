6e36d03e89772b34a4bb20f274595010b2dc4da4
keoh23 <kevin.oh23@gmail.com>
2016-08-17 16:55:49 -0700

    FAB creates an Alert Dialog, created layouts for 2nd Activities
    now trying to connect create action in FAB to connect to 2nd Activity

---
+++ ToDoApp/app/src/main/java/com/example/owen/todolist/AddedList.java
@@ -0,0 +1,7 @@
+package com.example.owen.todolist;
+
+/**
+ * Created by Owen on 8/17/2016.
+ */
+public class AddedList {
+}
--- ToDoApp/app/src/main/java/com/example/owen/todolist/MainActivity.java
+++ ToDoApp/app/src/main/java/com/example/owen/todolist/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.owen.todolist;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -35,6 +41,27 @@ public class MainActivity extends AppCompatActivity {
         myFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                final EditText input = new EditText(MainActivity.this);
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+
+                builder.setView(input);
+
+                builder.setMessage(R.string.change_the_text)
+                        .setPositiveButton(R.string.change, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent intent = new Intent(this, NewActivity.class);
+                                startActivity(intent);
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                            }
+                        });
+
+                Dialog dialog = builder.create();
+                dialog.show();
 
             }
         });
