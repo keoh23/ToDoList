c9eeec55a95301a80d12a183ff68678c37d178e8
keoh23 <kevin.oh23@gmail.com>
2016-08-18 11:58:09 -0700

    added blank readme and added android to gitignore, also changed FAB behavior to create items directly to main to do list

--- ToDoApp/app/src/main/java/com/example/owen/todolist/AddedList.java
+++
@@ -1,7 +0,0 @@
-package com.example.owen.todolist;
-
-/**
- * Created by Owen on 8/17/2016.
- */
-public class AddedList {
-}
--- ToDoApp/app/src/main/java/com/example/owen/todolist/MainActivity.java
+++ ToDoApp/app/src/main/java/com/example/owen/todolist/MainActivity.java
@@ -2,13 +2,13 @@ package com.example.owen.todolist;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -16,8 +16,9 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
-    private FloatingActionButton myFAB;
-    private ArrayList<MainList> maintodoList = new ArrayList<>();
+    private FloatingActionButton mainFAB;
+    private ArrayList<String> maintodoList = new ArrayList<>();
+    private ArrayAdapter<String> mainarrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,20 +26,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView = (ListView)findViewById(R.id.list_view1);
-        myFAB = (FloatingActionButton)findViewById(R.id.myFAB);
+        mainFAB = (FloatingActionButton)findViewById(R.id.mainFAB);
+        
+//        CustomBaseAdapter adapter = new CustomBaseAdapter(this, maintodoList);
+        mainarrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, maintodoList);
+        listView.setAdapter(mainarrayAdapter);
 
-        maintodoList.add(new MainList("Poetic Justice", "Kendrick Lamar"));
-        maintodoList.add(new MainList("Can I Kick It", "A Tribe Called Quest"));
-        maintodoList.add(new MainList("Sober", "Childish Gambino"));
-        maintodoList.add(new MainList("Brown Skin Lady", "Black Star"));
-        maintodoList.add(new MainList("G.O.M.D.", "J. Cole"));
-
-
-        CustomBaseAdapter adapter = new CustomBaseAdapter(this, maintodoList);
-
-        listView.setAdapter(adapter);
-
-        myFAB.setOnClickListener(new View.OnClickListener() {
+        mainFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
@@ -51,8 +45,11 @@ public class MainActivity extends AppCompatActivity {
                 builder.setMessage(R.string.change_the_text)
                         .setPositiveButton(R.string.change, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
-                                Intent intent = new Intent(this, NewActivity.class);
-                                startActivity(intent);
+                                maintodoList.add(input.getText().toString());
+                                mainarrayAdapter.notifyDataSetChanged();
+//                                Intent intent = new Intent(MainActivity.this, AddedActivity.class);
+//                                intent.putExtra("Title", "BACON");
+//                                startActivity(intent);
                             }
                         })
                         .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
