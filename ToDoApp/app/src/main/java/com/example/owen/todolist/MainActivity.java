09386003f846dc5377d1b46392b668b386e33c98
keoh23 <kevin.oh23@gmail.com>
2016-08-18 17:01:29 -0700

    got intents to work correctly by editing manifest
    had a lot of trial and error with deleting and making new class. turns
    out I wasn't creating java.class correctly (MUST REMEMBER FOR NEXT TIME)

--- ToDoApp/app/src/main/java/com/example/owen/todolist/MainActivity.java
+++ ToDoApp/app/src/main/java/com/example/owen/todolist/MainActivity.java
@@ -2,22 +2,24 @@ package com.example.owen.todolist;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private FloatingActionButton mainFAB;
-    private ArrayList<String> maintodoList = new ArrayList<>();
+    private LinkedList<String> maintodoList = new LinkedList<>();
     private ArrayAdapter<String> mainarrayAdapter;
 
     @Override
@@ -27,9 +29,9 @@ public class MainActivity extends AppCompatActivity {
 
         listView = (ListView)findViewById(R.id.list_view1);
         mainFAB = (FloatingActionButton)findViewById(R.id.mainFAB);
-        
-//        CustomBaseAdapter adapter = new CustomBaseAdapter(this, maintodoList);
-        mainarrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, maintodoList);
+
+//        final CustomBaseAdapter adapter = new CustomBaseAdapter(MainActivity.this,);
+        mainarrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, maintodoList);
         listView.setAdapter(mainarrayAdapter);
 
         mainFAB.setOnClickListener(new View.OnClickListener() {
@@ -47,9 +49,6 @@ public class MainActivity extends AppCompatActivity {
                             public void onClick(DialogInterface dialog, int id) {
                                 maintodoList.add(input.getText().toString());
                                 mainarrayAdapter.notifyDataSetChanged();
-//                                Intent intent = new Intent(MainActivity.this, AddedActivity.class);
-//                                intent.putExtra("Title", "BACON");
-//                                startActivity(intent);
                             }
                         })
                         .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
@@ -59,6 +58,14 @@ public class MainActivity extends AppCompatActivity {
 
                 Dialog dialog = builder.create();
                 dialog.show();
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+//                intent.putExtra("Title", maintodoList);
+                startActivity(intent);
 
             }
         });
---
+++ ToDoApp/app/src/main/java/com/example/owen/todolist/SecondActivity.java
@@ -0,0 +1,82 @@
+package com.example.owen.todolist;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Owen on 8/18/2016.
+ */
+public class SecondActivity extends AppCompatActivity {
+    private TextView addedtitleView;
+    private ListView addedlistView;
+    private FloatingActionButton addedFAB;
+    private ArrayList<String> addedtodoList = new ArrayList<>();
+    private ArrayAdapter<String> addedarrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_added);
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("Title");
+        addedtitleView.setText(title);
+
+        addedtitleView = (TextView)findViewById(R.id.added_title1);
+        addedlistView = (ListView)findViewById(R.id.added_list_view1);
+        addedFAB = (FloatingActionButton)findViewById(R.id.addedFAB);
+        addedarrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, addedtodoList);
+        addedlistView.setAdapter(addedarrayAdapter);
+
+        addedFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(SecondActivity.this);
+
+                final EditText input = new EditText(SecondActivity.this);
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+
+                builder.setView(input);
+
+                builder.setMessage("To Do!")
+                        .setPositiveButton(R.string.change, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                addedtodoList.add(input.getText().toString());
+                                addedarrayAdapter.notifyDataSetChanged();
+
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                            }
+                        });
+
+                Dialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+
+
+
+//        CustomBaseAdapter adapter = new CustomBaseAdapter(this, );
+//        addedlistView.setAdapter(adapter);
+
+    }
+
+
+}
+
